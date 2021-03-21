0422db9e2f7f2b05a3140697e543b633eefe73d7
Mosey <moseev.es@ya.ru>
2021-03-21 16:22:35 +0300

    express upgrade

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-require('http')
-.Server((req, res) => {
-    if (req.url === '/json') {
-      res.writeHead(200, {
-         'Content-Type': 'application/json; charset=utf-8',
-         'Access-Control-Allow-Origin': '*'
-      });
+const express = require('express');
+const app = express();
 
-      res.end('{"result": "правильный JSON"}');
-    }
-
-    if (req.url === '/zero') {
-      res.writeHead(404, {
-         'Content-Type': 'text/html; charset=utf-8'
-      });
-
-      res.end('<h1>Не найдено!</h1>');
-    }
-    res.writeHead(200, {
-      'Content-Type': 'text/javascript; charset=utf-8'
-    });
-    require('fs').createReadStream('./index.js').pipe(res);
- })
-.listen(80);
+app.listen(80, () => console.log(process.pid));
